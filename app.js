ff00052cc7cd80d34ccd7b0de896753036a11aaa
Nikita Averyanov <rac1onal@yandex.ru>
2023-06-28 19:39:47 +0300

    feat: отправлять post запросы с объектом (успешно)

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const express = require('express');
-const usersRouter = require('./routes/users');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const bodyParser = require('body-parser');
 
 const app = express();
 
+const usersRouter = require('./routes/users');
+
+// применить для всех роутов bodyParser (чтение тела запроса)
+app.use(bodyParser.json());
 app.use('/users', usersRouter);
 
 app.listen(3000, () => {
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const usersRouter = require('express').Router();
 
-// usersRouter.post('/', createUser);
-// eslint-disable-next-line no-console
-usersRouter.post('/', () => { console.log('Ес-с-сть запрос!'); });
+const { createUser } = require('../controllers/users');
+
+usersRouter.post('/', createUser);
 
 module.exports = usersRouter;
