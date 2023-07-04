613be639cc8e7ed28c956427bbe4d7b53e8d00f9
Nikita Averyanov <rac1onal@yandex.ru>
2023-07-04 21:22:25 +0300

    refactor: сделать поля name, about и avatar схемы 'user' необязательными, добавить значения по умолчанию

--- models/user.js
+++ models/user.js
@@ -6,19 +6,22 @@ const isEmail = require('validator/lib/isEmail');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'Поле "name" должно быть заполнено'],
+    required: false,
+    default: 'Жак-Ив Кусто',
     minlength: [2, 'Минимальная длина поля "name" - 2'],
     maxlength: [30, 'Максимальная длина поля "name" - 30'],
   },
   about: {
     type: String,
-    required: [true, 'Поле "about" должно быть заполнено'],
+    required: false,
+    default: 'Исследователь',
     minlength: [2, 'Минимальная длина поля "about" - 2'],
     maxlength: [30, 'Максимальная длина поля "about" - 30'],
   },
   avatar: {
     type: String,
-    required: [true, 'Поле "avatar" должно быть заполнено'],
+    required: false,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     validate: {
       validator: (v) => validator.isURL(v),
       message: 'Некорректный URL',
