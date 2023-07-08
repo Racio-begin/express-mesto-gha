03379a61339e7976a7e001e9079614b768f2571b
Nikita Averyanov <rac1onal@yandex.ru>
2023-07-09 01:05:14 +0300

    fix: поправить метод проверки почты и пароля в модели user

--- models/user.js
+++ models/user.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const validator = require('validator');
 const isEmail = require('validator/lib/isEmail');
+const UnauthorizedError = require('../errors/UnauthorizedError')
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -56,7 +57,7 @@ userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
-        return Promise.reject(new Error('Неправильные почта или пароль'));
+        return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
       }
       return bcrypt.compare(
         password,
@@ -64,7 +65,7 @@ userSchema.statics.findUserByCredentials = function (email, password) {
       )
         .then((matched) => {
           if (!matched) {
-            return Promise.reject(new Error('Неправильные почта или пароль'));
+            return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
           }
           return user;
         });
