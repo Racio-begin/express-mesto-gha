701cbd7096fcb09dd2da24ecd1f6965c0b4f76fd
Nikita Averyanov <rac1onal@yandex.ru>
2023-07-08 20:35:13 +0300

    fix: починить auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,18 +1,13 @@
 const jwt = require('jsonwebtoken');
 const SECRET_KEY = require('../utils/sk');
-
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
 const extractBearerToken = (header) => header.replace('Bearer ', '');
 
-// eslint-disable-next-line consistent-return
-const auth = (req, res, next) => {
-  // достаём авторизационный заголовок
+module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-
-  // убеждаемся, что он есть или начинается с Bearer
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return next(new UnauthorizedError('Необходима авторизация'));
+    throw new UnauthorizedError('Необходима авторизация!');
   }
 
   const token = extractBearerToken(authorization);
@@ -21,14 +16,10 @@ const auth = (req, res, next) => {
   try {
     payload = jwt.verify(token, SECRET_KEY);
   } catch (err) {
-    return next(new UnauthorizedError('Необходима авторизация'));
+    next(new UnauthorizedError('Необходима авторизация!'));
   }
 
-  // записываем пейлоуд в объект запроса
   req.user = payload;
 
-  // пропускаем запрос дальше
   next();
 };
-
-module.exports = { auth };
