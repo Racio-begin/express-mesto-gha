4038800b63766d1265952e96eaf01a0c60830a83
Nikita Averyanov <rac1onal@yandex.ru>
2023-07-09 17:12:20 +0300

    fix: убрать ограничение длины пароля в модели user

--- models/user.js
+++ models/user.js
@@ -46,7 +46,6 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     select: false,
-    minlength: 8,
   },
 }, {
   // добавим, чтобы при запросе не приходил пароль пользователя
